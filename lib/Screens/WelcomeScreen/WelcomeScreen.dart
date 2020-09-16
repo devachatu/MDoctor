5a2d0a5b12467a0bc01817ff7c8e84336bd818af
devachatu <deva.chaitanya.btech2018@sitpune.edu.in>
2020-09-16 13:56:09 +0530

    First commit

---
+++ lib/Screens/WelcomeScreen/WelcomeScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class WelcomeScreen extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff4749a0) ,
+
+    );
+  }
+
+}
